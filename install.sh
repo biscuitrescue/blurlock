0d4e242116ee52466b558f882a1b0a00a13e0046
biscuitrescue <67876678+biscuitrescue@users.noreply.github.com>
2021-09-09 13:05:47 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ if ls /usr/bin | grep -i doas
 then 
 	doas mv blurlock.sh /usr/bin/blurlock
 else
-	sudo mv blurlock.sh /usr/bin/blurlock.sh
+	sudo mv blurlock.sh /usr/bin/blurlock
 fi
 echo
 echo done
